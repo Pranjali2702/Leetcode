b7eaf8dc1c97b8d5d010ca97fc3eec1fd311473f
Pranjali2702 <pranjalibisht5@gmail.com>
2026-09-06 21:42:55 +0530

    Time: 0 ms (100%), Space: 87.1 MB (76.03%) - LeetHub

---
+++ 0860-lemonade-change/0860-lemonade-change.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int count5=0;
+        int count10=0;
+        int count20=0;
+        for(int i=0;i<bills.size();i++){
+            if(bills[i]==5){
+                count5++;
+            }else if(bills[i]==10){
+                if(count5==0){
+                    return false;
+                }else{
+                    count10++;
+                    count5--;
+                }
+            }else{
+                if(count5>0 && count10>0){
+                    count5--;
+                    count10--;
+                    count20++;
+                }else if(count5>=3){
+                    count5-=3;
+                    count20++;
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
